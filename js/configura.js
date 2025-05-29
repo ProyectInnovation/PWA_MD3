c3051f7a2b478b0d776a98e342b9d3055d1ec29e
ProyectInnovation <proyectinnovtion@gmail.com>
2025-05-28 22:36:02 -0600

    Add files via upload

--- js/configura.js
+++ js/configura.js
@@ -1,32 +1,13 @@
-// @ts-nocheck
-function mostrarChiste() {
-    const inputNombre = document.getElementById("nombreIntegrante");
-
-    if(inputNombre.value){
-        const nombre = inputNombre.value;
-        console.log(nombre);
-
-        switch (nombre) {
-            case 'Abigail':
-                alert("Chiste: ¿Qué hace una abeja en el gimnasio? ¡Zumba!");
-                break;
-            case 'Sherley':
-                alert("Chiste: ¿Cuál es el colmo de un electricista? Que su mujer se llame Luz y sus hijos le sigan la corriente.");
-                break;
-            case 'Ulises':
-                alert("Chiste: ¿Qué le dice un semáforo a otro? No me mires, que me pongo rojo.");
-                break;
-            case 'Aldair':
-                alert("Chiste: ¿Qué le dijo un cable a otro cable? Somos los intocables.");
-                break;
-            case 'Sebastián':
-                alert("Chiste: ¿Qué le dice una impresora a otra? ¿Esa hoja es tuya o es impresión mía?");
-                break;
-            case 'Evelin':
-                alert("Chiste: ¿Por qué los esqueletos son tan tranquilos? Porque nada les quita el sueño.");
-                break;
-        }
-    }else{
-        alert("Seleciona un integrante por favor, el campo esta vacio.")
-    }
-}
+/** Barra de navegación. */
+import "./nav-drw.js"
+import "./nav-tab-scrollable.js"
+import "./nav-tab-fixed.js"
+import "./nav-bar.js"
+/** Elementos utilizados */
+import "../lib/js/custom/md-menu-button.js"
+import "../lib/js/custom/md-options-menu.js"
+import "../lib/js/custom/md-overflow-button.js"
+import "../lib/js/custom/md-overflow-menu.js"
+import "../lib/js/custom/md-select-menu.js"
+import "../lib/js/custom/md-top-app-bar.js"
+import "../lib/js/custom/md-slider-field.js"
